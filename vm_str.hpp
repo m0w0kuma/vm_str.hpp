f540e4b4ca0a014e5ebaccf25901d022cf32556e
Monokuma <79499397+m0w0kuma@users.noreply.github.com>
2025-08-09 14:32:06 -0300

    support for utf-16 wide strings

--- vm_str.hpp
+++ vm_str.hpp
@@ -16,11 +16,12 @@ namespace VmStr {
 namespace Util {
 constexpr int digit_to_int(char c) { return c - '0'; }
 
+template <typename T>
 inline void _memcpy(volatile void *dst, const volatile void *src,
                     std::size_t size) {
-  auto *d = static_cast<volatile uint8_t *>(dst);
-  auto *s = static_cast<const volatile uint8_t *>(src);
-  for (std::size_t i = 0; i < size; ++i)
+  auto *d = static_cast<volatile T *>(dst);
+  auto *s = static_cast<const volatile T *>(src);
+  for (std::size_t i = 0; i < size / sizeof(T); ++i)
     d[i] = s[i];
 }
 
@@ -70,34 +71,34 @@ std::array<uint8_t, BYTECODE_MAX_SIZE> _ET3{};
 namespace Expression {
 
 #define DEFINE_EXPR(name, body)                                                \
-  VMSTR_INLINE constexpr uint8_t cexpr_##name(uint8_t n,                       \
-                                              [[maybe_unused]] uint8_t d) body \
+  template <typename T>                                                        \
+  constexpr T cexpr_##name(T n, [[maybe_unused]] T d) body                     \
                                                                                \
-      VMSTR_INLINE uint8_t                                                     \
-      name(uint8_t n, [[maybe_unused]] uint8_t d) body
+      template <typename T>                                                    \
+      VMSTR_INLINE T name(T n, [[maybe_unused]] T d) body
 
 DEFINE_EXPR(_xor, { return n ^ d; })
 DEFINE_EXPR(_not, { return ~n; })
 
 DEFINE_EXPR(rotl, {
-  constexpr uint8_t INT_BITS = std::numeric_limits<uint8_t>::digits;
+  constexpr T INT_BITS = std::numeric_limits<T>::digits;
   d %= INT_BITS;
   return (n << d) | (n >> (INT_BITS - d));
 })
 
 DEFINE_EXPR(rotr, {
-  constexpr uint8_t INT_BITS = std::numeric_limits<uint8_t>::digits;
+  constexpr T INT_BITS = std::numeric_limits<T>::digits;
   d %= INT_BITS;
   return (n >> d) | (n << (INT_BITS - d));
 })
 
-VMSTR_INLINE uint8_t alt_xor(uint8_t a, uint8_t b) {
+template <typename T> VMSTR_INLINE T alt_xor(T a, T b) {
   return ((((((~a | ~b) ^ -~a) + (2 * ((~a | ~b) & -~a))) & b) +
            ((((~a | ~b) ^ -~a) + (2 * ((~a | ~b) & -~a))) | b)) -
           ((((~a & ~b) & b) + ((~a & ~b) | b)) - ~a));
 }
 
-VMSTR_INLINE uint8_t alt_not(uint8_t a, uint8_t b) {
+template <typename T> VMSTR_INLINE T alt_not(T a, T b) {
   return (((~~a | ((((~a | b) - ~a) + ((a & ~b) + b)) -
                    (((~a | b) - ~a) + ((a & ~b) + b)))) -
            ~~a) +
@@ -125,13 +126,14 @@ enum OP : uint8_t {
 };
 
 #define INSERT(val)                                                            \
-  ret[vip] = val;                                                              \
+  ret[vip] = static_cast<T>(val);                                              \
   vip++;
 
-#define K table[vip]
+#define K static_cast<T>(table[vip])
 
-template <size_t N> consteval auto gen_bytecode(const char (&str)[N]) {
-  std::array<uint8_t, BYTECODE_MAX_SIZE> ret{};
+template <typename ST, typename T, size_t N>
+constexpr auto gen_bytecode(const ST (&str)[N]) {
+  std::array<T, BYTECODE_MAX_SIZE> ret{};
   auto table = Global::_ET2;
 
   int vip = 0;
@@ -139,49 +141,53 @@ template <size_t N> consteval auto gen_bytecode(const char (&str)[N]) {
     OP op = static_cast<OP>(Util::rand_int(Global::seed + i) %
                             (static_cast<uint8_t>(OP::END)));
     if (op == OP::XOR) {
-      uint8_t key = Util::rand_int(Global::seed + i) % 255;
-      auto val = Expression::cexpr__xor(str[i], key);
+      T key = static_cast<T>(Util::rand_int(Global::seed + i) %
+                             std::numeric_limits<T>::max());
+      T val = Expression::cexpr__xor<T>(str[i], key);
       INSERT(OP::PUSH);
       INSERT(val ^ K);
       INSERT(OP::PUSH);
-      INSERT(static_cast<char>(key) ^ K);
+      INSERT(static_cast<T>(key) ^ K);
       INSERT(OP::XOR);
       continue;
     } else if (op == OP::NOT) {
-      auto val = Expression::cexpr__not(str[i], 0);
+      T val = Expression::cexpr__not<T>(str[i], 0);
       INSERT(OP::PUSH);
       INSERT(val ^ K);
       INSERT(OP::NOT);
       continue;
     } else if (op == OP::ROTR) {
-      uint8_t key = Util::rand_int(Global::seed + i) % 255;
-      auto val = Expression::cexpr_rotr(str[i], key);
+      T key = static_cast<T>(Util::rand_int(Global::seed + i) %
+                             std::numeric_limits<T>::max());
+      T val = Expression::cexpr_rotr<T>(str[i], key);
       INSERT(OP::PUSH);
       INSERT(val ^ K);
       INSERT(OP::PUSH);
-      INSERT(static_cast<char>(key) ^ K);
+      INSERT(static_cast<T>(key) ^ K);
       INSERT(OP::ROTL);
       continue;
     } else if (op == OP::ROTL) {
-      uint8_t key = Util::rand_int(Global::seed + i) % 255;
-      auto val = Expression::cexpr_rotl(str[i], key);
+      T key = static_cast<T>(Util::rand_int(Global::seed + i) %
+                             std::numeric_limits<T>::max());
+      T val = Expression::cexpr_rotl<T>(str[i], key);
       INSERT(OP::PUSH);
       INSERT(val ^ K);
       INSERT(OP::PUSH);
-      INSERT(static_cast<char>(key) ^ K);
+      INSERT(static_cast<T>(key) ^ K);
       INSERT(OP::ROTR);
       continue;
     } else if (op == OP::ALT_XOR) {
-      uint8_t key = Util::rand_int(Global::seed + i) % 255;
-      auto val = Expression::cexpr__xor(str[i], key);
+      T key = static_cast<T>(Util::rand_int(Global::seed + i) %
+                             std::numeric_limits<T>::max());
+      T val = Expression::cexpr__xor<T>(str[i], key);
       INSERT(OP::PUSH);
       INSERT(val ^ K);
       INSERT(OP::PUSH);
-      INSERT(static_cast<char>(key) ^ K);
+      INSERT(static_cast<T>(key) ^ K);
       INSERT(OP::ALT_XOR);
       continue;
     } else if (op == OP::ALT_NOT) {
-      auto val = Expression::cexpr__not(str[i], 0);
+      auto val = Expression::cexpr__not<T>(str[i], 0);
       INSERT(OP::PUSH);
       INSERT(val ^ K);
       INSERT(OP::ALT_NOT);
@@ -197,72 +203,72 @@ template <size_t N> consteval auto gen_bytecode(const char (&str)[N]) {
   return ret;
 }
 
-#define CHAIN(offset) run<vip + offset, bytecode>(vsp, stack)
+#define CHAIN(offset) run<T, vip + offset, bytecode>(vsp, stack)
+#define VSP(x) x + opaque_var
 
-template <size_t vip, std::array bytecode>
-VMSTR_INLINE void run(volatile size_t &vsp, volatile uint8_t *stack) {
-  volatile auto opaque_var =
-      Expression::alt_xor(Global::_ET1[vip + 1], Global::_ET1[vip + 1]);
-
+template <typename T, size_t vip, std::array bytecode>
+VMSTR_INLINE void run(volatile size_t &vsp, volatile T *stack) {
+  volatile uint8_t opaque_var = Expression::alt_xor<uint8_t>(
+      Global::_ET1[vip + 1], Global::_ET1[vip + 1]);
   constexpr OP op = static_cast<OP>(bytecode[vip]);
   if constexpr (op == OP::PUSH) {
     Global::_ET3[vip + 1] = Global::_ET1[vip + 1];
     Global::_ET3[vip + 1] =
-        Expression::alt_xor(Global::_ET3[vip + 1], Global::GK);
-    volatile auto _key = Global::_ET3[vip + 1];
-    uint8_t imm = bytecode[vip + 1] ^ _key;
-    Util::_memcpy(stack + vsp, &imm, sizeof(imm));
-    vsp += sizeof(imm) + opaque_var;
+        Expression::alt_xor<uint8_t>(Global::_ET3[vip + 1], Global::GK);
+    volatile uint8_t _key = Global::_ET3[vip + 1];
+    T imm = bytecode[vip + 1] ^ static_cast<T>(_key);
+    Util::_memcpy<T>(stack + vsp, &imm, sizeof(T));
+    vsp += VSP(1);
     CHAIN(2);
   } else if constexpr (op == OP::XOR) {
-    volatile auto imm = stack[vsp - 2];
-    volatile auto key = stack[vsp - 1];
-    vsp -= sizeof(imm) + sizeof(key);
-    uint8_t xor_val = Expression::_xor(imm, (key + opaque_var));
-    Util::_memcpy(stack + vsp, &xor_val, sizeof(xor_val));
-    vsp += sizeof(xor_val) + opaque_var;
+    volatile T imm = stack[vsp - 2];
+    volatile T key = stack[vsp - 1];
+    vsp -= VSP(2);
+    T xor_val = Expression::_xor<T>(imm, (key + opaque_var));
+    Util::_memcpy<T>(stack + vsp, &xor_val, sizeof(T));
+    vsp += VSP(1);
     CHAIN(1);
   } else if constexpr (op == OP::NOT) {
-    volatile auto imm = stack[vsp - 1];
-    vsp -= sizeof(imm);
-    volatile uint8_t not_val = Expression::_not(imm + opaque_var, 0);
-    Util::_memcpy(stack + vsp, &not_val, sizeof(not_val));
-    vsp += sizeof(not_val) + opaque_var;
+    volatile T imm = stack[vsp - 1];
+    vsp -= VSP(1);
+    volatile T not_val = Expression::_not<T>(imm + opaque_var, 0);
+    Util::_memcpy<T>(stack + vsp, &not_val, sizeof(T));
+    vsp += VSP(1);
     CHAIN(1);
   } else if constexpr (op == OP::ROTR) {
-    volatile auto imm = stack[vsp - 2];
-    volatile auto key = stack[vsp - 1];
-    vsp -= sizeof(imm) + sizeof(key);
-    volatile uint8_t rotr_val = Expression::rotr(imm, key + opaque_var);
-    Util::_memcpy(stack + vsp, &rotr_val, sizeof(rotr_val));
-    vsp += sizeof(rotr_val) + opaque_var;
+    volatile T imm = stack[vsp - 2];
+    volatile T key = stack[vsp - 1];
+    vsp -= VSP(2);
+    volatile T rotr_val = Expression::rotr<T>(imm, key + opaque_var);
+    Util::_memcpy<T>(stack + vsp, &rotr_val, sizeof(T));
+    vsp += VSP(1);
     CHAIN(1);
   } else if constexpr (op == OP::ROTL) {
-    volatile auto imm = stack[vsp - 2];
-    volatile auto key = stack[vsp - 1];
-    vsp -= sizeof(imm) + sizeof(key);
-    volatile uint8_t rotl_val = Expression::rotl(imm, key + opaque_var);
-    Util::_memcpy(stack + vsp, &rotl_val, sizeof(rotl_val));
-    vsp += sizeof(rotl_val) + opaque_var;
+    volatile T imm = stack[vsp - 2];
+    volatile T key = stack[vsp - 1];
+    vsp -= VSP(2);
+    volatile T rotl_val = Expression::rotl<T>(imm, key + opaque_var);
+    Util::_memcpy<T>(stack + vsp, &rotl_val, sizeof(T));
+    vsp += VSP(1);
     CHAIN(1);
   } else if constexpr (op == OP::ALT_XOR) {
-    volatile auto imm = stack[vsp - 2];
-    volatile auto key = stack[vsp - 1];
-    vsp -= sizeof(imm) + sizeof(key);
-    volatile uint8_t mbaxor_val = Expression::alt_xor(imm, key + opaque_var);
-    Util::_memcpy(stack + vsp, &mbaxor_val, sizeof(mbaxor_val));
-    vsp += sizeof(mbaxor_val) + opaque_var;
+    volatile T imm = stack[vsp - 2];
+    volatile T key = stack[vsp - 1];
+    vsp -= VSP(2);
+    volatile T mbaxor_val = Expression::alt_xor<T>(imm, key + opaque_var);
+    Util::_memcpy<T>(stack + vsp, &mbaxor_val, sizeof(T));
+    vsp += VSP(1);
     CHAIN(1);
   } else if constexpr (op == OP::ALT_NOT) {
-    volatile auto imm = stack[vsp - 1];
-    vsp -= sizeof(imm);
-    volatile uint8_t not_val = Expression::alt_not(imm + opaque_var, imm);
-    Util::_memcpy(stack + vsp, &not_val, sizeof(not_val));
-    vsp += sizeof(not_val) + opaque_var;
+    volatile T imm = stack[vsp - 1];
+    vsp -= VSP(1);
+    volatile T not_val = Expression::alt_not<T>(imm + opaque_var, imm);
+    Util::_memcpy<T>(stack + vsp, &not_val, sizeof(T));
+    vsp += VSP(1);
     CHAIN(1);
   } else if constexpr (op == OP::TERMINATOR) {
-    uint8_t terminator = 0x0;
-    Util::_memcpy(stack + vsp, &terminator, sizeof(terminator));
+    T terminator = 0x0;
+    Util::_memcpy<T>(stack + vsp, &terminator, sizeof(T));
     return;
   } else {
     assert(false && "Invalid opcode");
@@ -272,7 +278,7 @@ VMSTR_INLINE void run(volatile size_t &vsp, volatile uint8_t *stack) {
 template <std::array bytecode> VMSTR_INLINE std::string exec_str() {
   volatile size_t vsp = 0;
   volatile uint8_t stack[STACK_MAX_SIZE];
-  run<0, bytecode>(vsp, stack);
+  run<uint8_t, 0, bytecode>(vsp, stack);
 
   uint8_t const *t = const_cast<uint8_t *>(stack);
   const char *t2 = reinterpret_cast<const char *>(t);
@@ -284,7 +290,7 @@ template <std::array bytecode> VMSTR_INLINE std::string exec_str() {
 template <std::array bytecode> VMSTR_INLINE const char *exec_cstr() {
   volatile size_t vsp = 0;
   static volatile uint8_t stack[STACK_MAX_SIZE];
-  run<0, bytecode>(vsp, stack);
+  run<uint8_t, 0, bytecode>(vsp, stack);
 
   uint8_t const *t = const_cast<uint8_t *>(stack);
   const char *t2 = reinterpret_cast<const char *>(t);
@@ -292,8 +298,38 @@ template <std::array bytecode> VMSTR_INLINE const char *exec_cstr() {
   return t2;
 }
 
+template <std::array bytecode> VMSTR_INLINE std::wstring exec_wstr() {
+  volatile size_t vsp = 0;
+  volatile uint16_t stack[STACK_MAX_SIZE];
+  run<uint16_t, 0, bytecode>(vsp, stack);
+
+  uint16_t const *t = const_cast<uint16_t *>(stack);
+  const wchar_t *t2 = reinterpret_cast<const wchar_t *>(t);
+  auto str = std::wstring(t2);
+
+  return str;
+}
+
+template <std::array bytecode> VMSTR_INLINE const wchar_t *exec_cwstr() {
+  volatile size_t vsp = 0;
+  static volatile uint16_t stack[STACK_MAX_SIZE];
+  run<uint16_t, 0, bytecode>(vsp, stack);
+
+  uint16_t const *t = const_cast<uint16_t *>(stack);
+  const wchar_t *t2 = reinterpret_cast<const wchar_t *>(t);
+
+  return t2;
+}
+
 } // namespace Vm
 } // namespace VmStr
 
-#define vm_str(str) VmStr::Vm::exec_str<VmStr::Vm::gen_bytecode(str)>()
-#define vm_cstr(str) VmStr::Vm::exec_cstr<VmStr::Vm::gen_bytecode(str)>()
+#define VM_STR(str)                                                            \
+  VmStr::Vm::exec_str<VmStr::Vm::gen_bytecode<char, uint8_t>(str)>()
+#define VM_CSTR(str)                                                           \
+  VmStr::Vm::exec_cstr<VmStr::Vm::gen_bytecode<char, uint8_t>(str)>()
+
+#define VM_W_STR(wstr)                                                         \
+  VmStr::Vm::exec_wstr<VmStr::Vm::gen_bytecode<wchar_t, uint16_t>(wstr)>()
+#define VM_W_CSTR(wstr)                                                        \
+  VmStr::Vm::exec_cwstr<VmStr::Vm::gen_bytecode<wchar_t, uint16_t>(wstr)>()
